72bc535d47426e102f4313d166d5f5ba8e2eecef
Sus-xl <55488310+Sus-xl@users.noreply.github.com>
2019-10-01 22:04:52 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,38 @@
-function preload(){
+var colorList1 = ['#f15c4f',
+                 '#fbb040',
+                 '#ededed',
+                 '#ffffff'];
+
+var colorList2 = ['#6ed689',
+                 '#aeffc3',
+                 '#299946',
+                 '#ffffff'];
+function preload() {
   // put preload code here
 }
 
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  // put drawing code here
+  frameRate(10);
+  background(70);
+  for (var x = 0; x < width+20; x += 20) {
+    for (var y = 0; y < height; y += 20) {
+      noStroke();
+
+      if (mouseX > windowWidth/2) {
+        fill(color(random(colorList1)));
+      } else {
+        fill(color(random(colorList2)));
+      }
+
+      quad(x-20, y, x, y, x + 20, y+20, x, y + 20);
+      smooth();
+
+
+    }
+  }
 }
